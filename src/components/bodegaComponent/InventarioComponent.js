5a79f57cea34ee50fc00368cb5f88999cf8de406
N3hz <Daniel5122@gmail.com>
2021-10-31 22:28:29 -0300

    Comentarios

--- src/components/bodegaComponent/InventarioComponent.js
+++ src/components/bodegaComponent/InventarioComponent.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React from 'react';
 import noticias from '../../img/noticias.png';
+import oc from '../../img/oc.png';
+import ventaOnline from '../../img/ventaOnline.png';
+import despachoSucursal from '../../img/despachoSucursal.png';
+import ajusteInventario from '../../img/ajusteInventario.png';
 import { subMenuBodegas } from '../../types/pagesInicioTypes';
+
 
 export const InventarioComponent = ({parentStateSetter}) => {
     return (
@@ -12,7 +17,7 @@ export const InventarioComponent = ({parentStateSetter}) => {
                             <p className="fondoTituloSup">Ingresos por OC</p>
                         </div>
                         <div className="row">
-                            <img className="imgBloques" src={noticias} alt="oc" onClick={()=>{ parentStateSetter(subMenuBodegas.subMenuIngresoOC) }} />
+                            <img className="imgBloques" src={oc} alt="oc" onClick={()=>{ parentStateSetter(subMenuBodegas.subMenuIngresoOC) }} />
                         </div>
                     </div>
                 </div>
@@ -22,7 +27,7 @@ export const InventarioComponent = ({parentStateSetter}) => {
                             <p className="fondoTituloSup"> Ajustes de Inventario</p>
                         </div>
                         <div className="row">
-                            <img className="imgBloques" src={noticias} alt="ajustes" onClick={()=>{ parentStateSetter(subMenuBodegas.subAjusteInventario) }} />
+                            <img className="imgBloques" src={ajusteInventario} alt="ajustes" onClick={()=>{ parentStateSetter(subMenuBodegas.subAjusteInventario) }} />
                         </div>
                     </div>
                 </div>
@@ -32,7 +37,7 @@ export const InventarioComponent = ({parentStateSetter}) => {
                             <p className="fondoTituloSup"> Venta Online</p>
                         </div>
                         <div className="row">
-                            <img className="imgBloques" src={noticias} alt="ventaON" onClick={()=>{ parentStateSetter(subMenuBodegas.ventaOnline) }}/>
+                            <img className="imgBloques" src={ventaOnline} alt="ventaON" onClick={()=>{ parentStateSetter(subMenuBodegas.ventaOnline) }}/>
                         </div>
                     </div>
                 </div>
@@ -42,7 +47,7 @@ export const InventarioComponent = ({parentStateSetter}) => {
                             <p className="fondoTituloSup"> Despacho a Sucursal</p>
                         </div>
                         <div className="row">
-                            <img className="imgBloques" src={noticias} alt="despacho" onClick={()=>{ parentStateSetter(subMenuBodegas.subDespachoASucursal) }} />
+                            <img className="imgBloques" src={despachoSucursal} alt="despacho" onClick={()=>{ parentStateSetter(subMenuBodegas.subDespachoASucursal) }} />
                         </div>
                     </div>
                 </div>
--- src/components/bodegaComponent/SmIngresoOc.js
+++ src/components/bodegaComponent/SmIngresoOc.js
@@ -2,11 +2,93 @@ import React from 'react'
 import { subMenuBodegas } from '../../types/pagesInicioTypes';
 import atras from '../../img/atras.png';
 
-export const SmIngresoOc = ({parentStateSetter}) => {
+export const SmIngresoOc = ({ parentStateSetter }) => {
     return (
         <div className="container">
-            <img className="imgBloques" src={atras} alt="atrás" onClick={() => { parentStateSetter(subMenuBodegas.inventario) }} />
-            <p>Ingreso por OC</p>
-        </div>
+            <div className="DgTabla">
+                <div className='row'>
+                    <div className='col-xl-12 col-lg-12 col-td'>
+                        INGRESO POR ORDEN DE COMPRA
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        N° Orden
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        <input className='form-control input-xs' />
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        Fecha Orden
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        <input className='form-control input-xs' />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        Proveedor
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        <input className='form-control input-xs' />
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        Fecha Entrega
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        <input className='form-control input-xs' />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        Bodega Ingreso
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        <input className='form-control input-xs' />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        Detalles
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        <textarea id="oldText" name="oldText" rows="6" cols="22"></textarea>
+                    </div>
+                </div>
+                <div className='row'>
+                    <hr />
+                </div>
+                <div className='row'>
+                    <div className='col-xl-8 col-lg-8 col-td'>
+                        DETALLE
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        Código Producto
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        <input className='form-control input-xs' />
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        Cantidad
+                    </div>
+                    <div className='col-xl-2 col-lg-6 col-td'>
+                        <input className='form-control input-xs' />
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className="col">
+                        <table className="tableLeft" border="1">
+                            <thead>
+                                <tr>
+                                    <td>Código</td>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
